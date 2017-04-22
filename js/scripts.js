68074b3ba21725144e8f6ea4d2b8d11de1c04ff9
nescomtm <tomczyk@nescom.co>
2017-04-22 21:53:38 +0200

    zadanko z javascript 8.5

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+﻿// var name = prompt('Wpisz Swoje Imię');
+// alert('Witaj, ' + name);
+
+// console.log('Witaj, ' + name);
+
+
+
+var triangleArea;
+var a = prompt('Podaj Wartość dla : a');
+var h = prompt('Podaj Wartość dla : h');
+
+triangleArea = a*h/2;
+
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea); 
